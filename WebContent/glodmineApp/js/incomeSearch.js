3cd2ca31e7c89a0e7b125d7d7722ae5d262336f7
zhoutanxin <zhoutanxin@hotmail.com>
2015-10-20 16:16:16 +0800

    去掉x轴滚动

--- WebContent/glodmineApp/js/incomeSearch.js
+++ WebContent/glodmineApp/js/incomeSearch.js
@@ -29,7 +29,7 @@ $(window).scroll( function() {
 
     } 
 }); 
-$('body').perfectScrollbar({minScrollbarLength:100});
+$('body').perfectScrollbar({suppressScrollX:true});
 $("#incomeSearch").on("pageshow",function(e){
 	buildIncomeType();
 	quryList4Income();
